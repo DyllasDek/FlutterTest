86d9e85a106d31373449f354e144264b6e66b332
DyllasDek <danchik.korneenko@yandex.ru>
2022-11-16 19:14:10 +0300

    k

---
+++ dartTest/dart_application/bin/dart_application.dart
@@ -0,0 +1,39 @@
+//import 'dart:ffi';
+
+//import 'package:dart_application/dart_application.dart' as dart_application;
+
+void main() {
+  var bob = User();
+}
+
+class User {
+  String? name;
+  int? age;
+  // [] - Необязательные
+  //User(name,[age,...])
+}
+
+
+
+/*
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application.calculate()}!');
+
+  dynamic data = 5;
+  data = false;
+  print(data);
+  // Целочисленное деление
+  print('~/ : ${5 ~/ 3}');
+
+  var res = 5 == 5 ? 10 : 20;
+  print(res);
+
+  List nums = [5, 6, 7, 'hello', true];
+  nums.add('lox');
+  nums.addAll([1, 2, 3, 4]);
+  print(nums);
+  //removeAt(index)
+  //removeWhere(func)
+  //set -> var dig = {...};
+}
+*/
